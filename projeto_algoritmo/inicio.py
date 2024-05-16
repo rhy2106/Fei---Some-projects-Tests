f9ae9b0b77114a06ba1b26fea1141441da3bcce9
rhy2106 <79146870+rhy2106@users.noreply.github.com>
2024-05-16 01:44:53 -0300

    Update inicio.py

--- projeto_algoritmo/inicio.py
+++ projeto_algoritmo/inicio.py
@@ -1,82 +1,38 @@
-from random import randint
+import time
 def logar():
     while True:
         login = open("usuarios.txt", "r")
-        cpf1 = input("Digite o seu CPF: ")
+        cpf = input("Digite o seu CPF: ")
         senha = input("Digite a senha: ")
-        erro = 0
-        for linha in login.readlines():
-            erro += 1
-            cpf = ""
-            senha1 = ""
-            for i in range(11):
-                cpf += linha[7+i]
-            for i in range(6):
-                senha1 += linha[26+i]
-            if cpf1 == cpf and senha == senha1:
+        linhas = login.readlines()
+        for linha in linhas:
+            cpf1 = linha[7:18]
+            senha1 = linha[26:32]
+            if cpf == cpf1 and senha == senha1:
                 print("Login realizado com sucesso")
+                login.close()
                 return cpf
-        if erro != 0:
-            print("login falhou, login ou senha errada")
-            login.close()
+        print("login falhou, login ou senha errada")
+        login.close()
+# ========================================================== #
+def verificarCpf(cpf,linha):
+    return linha[5:16] == cpf
 # ========================================================== #
 def saldo(cpf):
     contas = open("contas.txt", "r")
-    nome = ""
-    real = ""
-    btc = ""
-    eth = ""
-    xrp = ""
-    erro = 1
-    for linha in contas.readlines():
-        tmp = ""
-        if erro == 1:
-            for i in range(11):
-                tmp += linha[5 + i]
-            if tmp == cpf:
-                i = 23
-                while True:
-                    if i < len(linha)-1:
-                        if linha[i] == "_":
-                            nome += " "
-                        else:
-                            nome += linha[i]
-                    else:
-                        break
-                    i += 1
-                erro = 0
-            else:
-                erro += 1
-        elif erro == 0:
-            if linha[0] == 'R':
-                i = 6
-                while i != len(linha)-1:
-                    real += linha[i]
-                    i += 1
-
-            elif linha[0] == 'B':
-                i = 5
-                while i != len(linha)-1:
-                    btc += linha[i]
-                    i += 1
-            elif linha[0] == 'E':
-                i = 5
-                while i != len(linha)-1:
-                    eth += linha[i]
-                    i += 1
-            elif linha[0] == 'X':
-                i = 5
-                while i != len(linha)-1:
-                    xrp += linha[i]
-                    i += 1
-                erro = 6
-        elif erro == 6:
-            erro = 1
-        else:
-            erro += 1
+    linhas = contas.readlines()
+    i = 0
+    while i < len(linhas):
+        if not verificarCpf(cpf,linhas[i]):
+            i += 6
             continue
+        nome = linhas[i][23:-1]
+        real = linhas[i+1][6:-1]
+        btc = linhas[i+2][5:-1]
+        eth = linhas[i+3][5:-1]
+        xrp = linhas[i+4][5:-1]
+        break
     print(f"Nome: {nome}")
-
     print("CPF: ",end="")
     for i in range(11):
         print(cpf[i],end="")
@@ -92,379 +48,173 @@ def saldo(cpf):
 # ========================================================== #
 def depositar(cpf):
     contas = open("contas.txt", "r")
-    lista = []
-    real = ""
-    real1 = float((input("Digite o valor em Real a ser depositado: ")))
-    erro = 1
-    for linha in contas.readlines():
-        tmp = ""
-        if erro == 1:
-            lista.append(linha)
-            for i in range(11):
-                tmp += linha[5 + i]
-            if tmp == cpf:
-                erro = 0
-            else:
-                erro += 1
-        elif erro == 0:
-            if linha[0] == 'R':
-                i = 6
-                while i != len(linha)-1:
-                    real += linha[i]
-                    i += 1
-                real = float(real)
-                real += real1
-                lista.append(f"REAL: {real:.2f}\n")
-            elif linha[0] == 'X':
-                lista.append(linha)
-                erro = 6
-            else:
-                lista.append(linha)
-        elif erro == 6:
-            lista.append(linha)
-            erro = 1
-        else:
-            lista.append(linha)
-            erro += 1
+    while True:
+        real = float((input("Digite o valor em Real a ser depositado: ")))
+        if real >= 0:
+            break
+        print("Valor Invalido")
+    linhas = contas.readlines()
+    i = 0
+    while i < len(linhas):
+        if not verificarCpf(cpf,linhas[i]):
+            i += 6
             continue
-    
+        real += float(linhas[i+1][6:-1])
+        linhas[i+1] = f"REAL: {real:.2f}\n"
+        break
     contas = open("contas.txt", "w")
-
-    for i in range(len(lista)):
-        contas.write(lista[i])
-    
+    for linha in linhas:
+        contas.write(linha)
     contas.close()
 # ========================================================== #
 def sacar(cpf):
     contas = open("contas.txt", "r")
-    lista = []
-    real = ""
-    erro = 1
-    for linha in contas.readlines():
-        tmp = ""
-        if erro == 1:
-            lista.append(linha)
-            for i in range(11):
-                tmp += linha[5 + i]
-            if tmp == cpf:
-                erro = 0
-            else:
-                erro += 1
-        elif erro == 0:
-            if linha[0] == 'R':
-                i = 6
-                while i != len(linha)-1:
-                    real += linha[i]
-                    i += 1
-                real = float(real)
-                while True:
-                    real1 = float((input("Digite o valor em Real a ser sacado: ")))
-                    if real < real1:
-                        print("impossivel realizar esta ação, digite um novo valor:")
-                    else:
-                        break
-                real -= real1
-                lista.append(f"REAL: {real:.2f}\n")
-            elif linha[0] == 'X':
-                lista.append(linha)
-                erro = 6
-            else:
-                lista.append(linha)
-        elif erro == 6:
-            lista.append(linha)
-            erro = 1
-        else:
-            lista.append(linha)
-            erro += 1
+    linhas = contas.readlines()
+    i = 0
+    while i < len(linhas):
+        if not verificarCpf(cpf,linhas[i]):
+            i += 6
             continue
-    
+        real = float(linhas[i+1][6:-1])
+        while True:
+            real1 = float((input("Digite o valor em Real a ser sacado: ")))
+            if real < real1:
+                print("impossivel realizar esta ação")
+            else:
+                break
+        real -= real1
+        linhas[i+1] = f"REAL: {real:.2f}\n"
+        contas.close()
+        break
     contas = open("contas.txt", "w")
-
-    for i in range(len(lista)):
-        contas.write(lista[i])
-    
+    for linha in linhas:
+        contas.write(linha)
     contas.close()
 # ========================================================== #
 def comprar(cpf):
-    moeda = input("Digite a moeda desejada (btc, eth ou xrp): ")
+    moeda = input("Digite a moeda desejada (BTC, ETH ou XRP): ")
     contas = open("contas.txt", "r")
     moedas = open("moedas.txt", "r")
-    lista = []
-    real = ""
-    real1 = 0
-    valor = ""
-    ct = ""
-    txc = ""
-    erro = 1
+    linhas = contas.readlines()
     for linha in moedas.readlines():
-        if (linha[0] == 'X' and moeda == "xrp")or(linha[0] == 'B' and moeda == "btc")or(linha[0] == 'E' and moeda == "eth"):
-            comeco = ""
-            for i in range(9):
-                comeco += linha[i]
-            i = 9
-            while linha[i] != " ":
-                ct += linha[i]
-                i += 1
-            j =  i + 6
-            while linha[j] != "%":
-                txc += linha[j]
-                j += 1
-            txc = float(txc)
-            resto = ""
-            while i != len(linha)-1:
-                resto += linha[i]
-                i += 1
-            ct = float(ct)
+        if (linha[0] == 'X' and moeda == "XRP")or(linha[0] == 'B' and moeda == "BTC")or(linha[0] == 'E' and moeda == "ETH"):
+            l = linha.split(" ")
+            txc = float(l[4])
+            ct = float(l[2])
     moedas.close()
-    for linha in contas.readlines():
-        tmp = ""
-        if erro == 1:
-            lista.append(linha)
-            for i in range(11):
-                tmp += linha[5 + i]
-            if tmp == cpf:
-                erro = 0
-            else:
-                erro += 1
-        elif erro == 0:
-            if linha[0] == 'R':
-                i = 6
-                while i != len(linha)-1:
-                    real += linha[i]
-                    i += 1
-                real = float(real)
-                while True:
-                    real1 = float((input("Digite o valor em reais da compra: ")))
-                    if real < real1:
-                        print("impossivel realizar esta ação, digite um novo valor:")
-                    else:
-                        break
-                real -= real1
-                lista.append(f"REAL: {real:.2f}\n")
-            elif linha[0] == 'B' and moeda == "btc":
-                i = 5
-                while i != len(linha)-1:
-                    valor += linha[i]
-                    i += 1
-                valor = float(valor)
-                valor += real1/ct
-                valor -= valor*txc
-                lista.append(f"BTC: {valor:.3f}\n")
-            elif linha[0] == 'E' and moeda == "eth":
-                i = 5
-                while i != len(linha)-1:
-                    valor += linha[i]
-                    i += 1
-                valor = float(valor)
-                valor += real1/ct
-                lista.append(f"ETH: {valor:.3f}\n")
-            elif linha[0] == 'X' and moeda == "xrp":
-                i = 5
-                while i != len(linha)-1:
-                    valor += linha[i]
-                    i += 1
-                valor = float(valor)
-                valor += real1/ct
-                lista.append(f"XRP: {valor:.3f}\n")
-                erro = 6
-            elif linha[0] == 'X':
-                lista.append(linha)
-                erro = 6
-            else:
-                lista.append(linha)
-        elif erro == 6:
-            lista.append(linha)
-            erro = 1
-        else:
-            lista.append(linha)
-            erro += 1
+    i = 0
+    while i < len(linhas):
+        if not verificarCpf(cpf,linhas[i]):
+            i += 6
             continue
-    
+        real = float(linhas[i+1][6:-1])
+        while True:
+            real1 = float((input("Digite o valor em reais da compra: ")))
+            if real < real1:
+                print("impossivel realizar esta ação, digite um novo valor:")
+            else:
+                break
+        real -= real1
+        linhas[i+1] =f"REAL: {real:.2f}\n"
+        if moeda == "BTC":
+            j = 2
+        elif moeda == "ETH":
+            j = 3
+        elif moeda == "XRP":
+            j = 4
+        valor = (float(linhas[i+j][5:-1])) + (real1/ct)*(1-txc)
+        linhas[i+j] = f"{moeda}: {valor:.3f}\n"
+        break
     contas = open("contas.txt", "w")
-
-    for i in range(len(lista)):
-        contas.write(lista[i])
-    
+    for linha in linhas:
+        contas.write(linha)
     contas.close()
 # ========================================================== #
 def vender(cpf):
-    moeda = input("Digite a moeda desejada (btc, eth ou xrp): ")
+    moeda = input("Digite a moeda desejada (BTC, ETH ou XRP): ")
     contas = open("contas.txt", "r")
     moedas = open("moedas.txt", "r")
-    lista = []
-    real = ""
-    real1 = 0
-    cont = -1
-    valor = ""
-    valor1 = 0
-    ct = ""
-    txv = ""
-    erro = 1
+    linhas = contas.readlines()
     for linha in moedas.readlines():
-        if (linha[0] == 'X' and moeda == "xrp")or(linha[0] == 'B' and moeda == "btc")or(linha[0] == 'E' and moeda == "eth"):
-            comeco = ""
-            for i in range(9):
-                comeco += linha[i]
-            i = 9
-            while linha[i] != " ":
-                ct += linha[i]
-                i += 1
-            j =  i + 17
-            while linha[j] != "%":
-                txv += linha[j]
-                j += 1
-            txv = float(txv)
-            resto = ""
-            while i != len(linha)-1:
-                resto += linha[i]
-                i += 1
-            ct = float(ct)
+        if (linha[0] == 'X' and moeda == "XRP")or(linha[0] == 'B' and moeda == "BTC")or(linha[0] == 'E' and moeda == "ETH"):
+            l = linha.split(" ")
+            txv = float(l[6])
+            ct = float(l[2])
     moedas.close()
-    for linha in contas.readlines():
-        if real1 == 0:
-            cont += 1
-        tmp = ""
-        if erro == 1:
-            lista.append(linha)
-            for i in range(11):
-                tmp += linha[5 + i]
-            if tmp == cpf:
-                erro = 0
-            else:
-                erro += 1
-        elif erro == 0:
-            if linha[0] == 'R':
-                i = 6
-                while i != len(linha)-1:
-                    real += linha[i]
-                    i += 1
-                real = float(real)
-                real1 = cont
-                lista.append(linha)
-            elif linha[0] == 'B' and moeda == "btc":
-                i = 5
-                while i != len(linha)-1:
-                    valor += linha[i]
-                    i += 1
-                valor = float(valor)
-                while True:
-                    valor1 = float((input(f"Digite o valor em {moeda} da venda: ")))
-                    if valor < valor1:
-                        print("impossivel realizar esta ação, digite um novo valor:")
-                    else:
-                        break
-                valor -= valor1
-                real = real + valor1*ct
-                real -= real*txv
-                lista[real1] = f"REAL: {real:.2f}\n"
-                lista.append(f"BTC: {valor:.3f}\n")
-            elif linha[0] == 'E' and moeda == "eth":
-                i = 5
-                while i != len(linha)-1:
-                    valor += linha[i]
-                    i += 1
-                valor = float(valor)
-                while True:
-                    valor1 = float((input(f"Digite o valor em {moeda} da venda: ")))
-                    if valor < valor1:
-                        print("impossivel realizar esta ação, digite um novo valor:")
-                    else:
-                        break
-                valor -= valor1
-                real = real + valor1*ct
-                real -= real*txv
-                lista[real1] = f"REAL: {real:.2f}\n"
-                lista.append(f"ETH: {valor:.3f}\n")
-            elif linha[0] == 'X' and moeda == "xrp":
-                i = 5
-                while i != len(linha)-1:
-                    valor += linha[i]
-                    i += 1
-                valor = float(valor)
-                while True:
-                    valor1 = float((input(f"Digite o valor em {moeda} da venda: ")))
-                    if valor < valor1:
-                        print("impossivel realizar esta ação, digite um novo valor:")
-                    else:
-                        break
-                valor -= valor1
-                real = real + valor1*ct
-                real -= real*txv
-                lista[real1] = f"REAL: {real:.2f}\n"
-                lista.append(f"XRP: {valor:.3f}\n")
-                erro = 6
-            elif linha[0] == 'X':
-                lista.append(linha)
-                erro = 6
-            else:
-                lista.append(linha)
-        elif erro == 6:
-            lista.append(linha)
-            erro = 1
-        else:
-            lista.append(linha)
-            erro += 1
+    i = 0
+    while i < len(linhas):
+        if not verificarCpf(cpf,linhas[i]):
+            i += 6
             continue
-    
+        real = float(linhas[i+1][6:-1])
+        if moeda == "BTC":
+            j = 2
+        elif moeda == "ETH":
+            j = 3
+        elif moeda == "XRP":
+            j = 4
+        valor = float(linhas[i+j][5:-1])
+        while True:
+            valor1 = float((input(f"Digite o valor em {moeda} da venda: ")))
+            if valor < valor1:
+                print("impossivel realizar esta ação, digite um novo valor:")
+            else:
+                valor -= valor1
+                linhas[i+j] = f"{moeda}: {valor:.3f}\n"
+                break
+        real = real + valor1*ct - (real + valor1*ct)*txv
+        linhas[i+1] = f"REAL: {real:.2f}\n"
+        break
     contas = open("contas.txt", "w")
-
-    for i in range(len(lista)):
-        contas.write(lista[i])
-    
+    for linha in linhas:
+        contas.write(linha)
     contas.close()
 # ========================================================== #
 def atualizar():
+    from random import randint
     moedas = open("moedas.txt", "r")
-    lista2 = []
-    for linha in moedas.readlines():
-        ct = ""
-        comeco = ""
-        for i in range(9):
-            comeco += linha[i]
-        i = 9
-        while linha[i] != " ":
-            ct += linha[i]
-            i += 1
-        resto = ""
-        while i != len(linha)-1:
-            resto += linha[i]
-            i += 1
-        ct = float(ct)
-        ct += ct*(randint(-5,5)/100)
-        lista2.append(f"{comeco}{ct:.2f}{resto}\n")
+    linhas = moedas.readlines()
+    i = 0
+    while i < len(linhas):
+        l = linhas[i].split(" ")
+        ct = float(l[2])
+        l[2] = f"{(ct + ct*(randint(-5,5)/100)):.2f}"
+        linhas[i] = " ".join(l)
+        i += 1
     moedas.close()
     moedas = open("moedas.txt", "w")
-    for i in range(len(lista2)):
-        moedas.write(lista2[i])
+    for linha in linhas:
+        moedas.write(linha)
     moedas.close()
 # ========================================================== #
 def extrato(cpf):
     extrato = open("extrato.txt","r")
-    erro = 0
-    nome = ""
-    for linha in extrato.readlines():
-        cpf1 = ""
-        if erro == 0:
-            for i in range(5,16):
-                cpf1 += linha[i]
-            if cpf1 == cpf:
-                i = 23
-                while i < len(linha)-1:
-                    nome += linha[i]
-                    i += 1
-                erro = -1
-            else:
-                erro = 1
-        elif erro == -1:
-            if linha[0] != "\n":
-                print(linha,end="")
+    linhas = extrato.readlines()
+    i = 0
+    lista = []
+    while i < len(linhas):
+        if linhas[i] == "\n":
+            lista.append(i)
+        i += 1
+    i = 0
+    while i < len(linhas):
+        if not verificarCpf(cpf,linhas[i]):
+            i = lista[0] + 1
+            lista.pop(0)
+            continue
+        nome = linhas[i][23:-1]
+        print(f"Nome: {nome}")
+        print(f"CPF: {cpf}")
+        for i in range(i+1,len(linhas)):
+            if linhas[i][0] != "\n":
+                print(linhas[i],end="")
             else:
                 break
-        else:
-            erro += 1
-            if linha[0] == "\n":
-                erro = 0
+        break
     extrato.close()
+# ========================================================== #
+def adicionarExtrato(cpf):
+    pass
 # ========================================================== #
 def menu(cpf):
     while True:
